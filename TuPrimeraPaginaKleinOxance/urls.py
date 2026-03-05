136bf54dd1c0c04b2222a8f4736b1ed1add36313
Noe Klein Oxance <noeklein.oxance@gmial.com>
2026-03-05 12:04:11 -0300

    correcion navbar y logout

--- TuPrimeraPaginaKleinOxance/urls.py
+++ TuPrimeraPaginaKleinOxance/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
+    path(
+        "accounts/logout/",
+        LogoutView.as_view(template_name="registration/logout.html"),
+        name="logout",
+    ),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
